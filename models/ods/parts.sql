d824d296b410303cc63ef687252a1a16a9df2cda
kh.haresh@gmail.com <kh.haresh@gmail.com>
2024-04-04 10:33:38 +0000

    🔁 Modified     - Model        - base_customer        - fct_orders_items        - parts_suppliers        - dim_part_supplier_xrf        - rpt_pricing_summary        - parts        - orders_items        - fct_orders        - rpt_minimum_cost_suppliers

--- models/ods/parts.sql
+++ models/ods/parts.sql
@@ -1,23 +1,27 @@
 {{
-    config(
-        materialized = 'table'
-    )
+  config({    
+    "materialized": "table"
+  })
 }}
-with parts as (
 
-    select * from {{ ref('base_part') }}
+WITH parts AS (
+
+  SELECT * 
+  
+  FROM {{ ref('base_part')}}
 
 )
-select 
-    p.part_key,
-    p.part_name,
-    p.part_manufacturer_name,
-    p.part_brand_name,
-    p.part_type_name,
-    p.part_size,
-    p.part_container_desc,
-    p.retail_price
-from
-    parts p
-order by
-    p.part_key
+
+SELECT 
+  p.part_key,
+  p.part_name,
+  p.part_manufacturer_name,
+  p.part_brand_name,
+  p.part_type_name,
+  p.part_size,
+  p.part_container_desc,
+  p.retail_price
+
+FROM parts AS p
+
+ORDER BY p.part_key
--- models/wh/rpt_pricing_summary.sql
+++ models/wh/rpt_pricing_summary.sql
@@ -1,31 +1,36 @@
 {{
-    config(
-        materialized = 'table'
-    )
+  config({    
+    "materialized": "table"
+  })
 }}
 
+WITH fct_orders_items AS (
+
+  SELECT * 
+  
+  FROM {{ ref('fct_orders_items')}}
+
+)
+
 /*
-Per TPC-H Spec: 
+Per TPC-H Spec:
 2.4.1 Pricing Summary Report Query (Q1)
 */
+SELECT 
+  f.return_status_code,
+  f.order_line_status_code,
+  sum(f.quantity) AS quantity,
+  sum(f.gross_item_sales_amount) AS gross_item_sales_amount,
+  sum(f.discounted_item_sales_amount) AS discounted_item_sales_amount,
+  sum(f.net_item_sales_amount) AS net_item_sales_amount,
+  avg(f.quantity) AS avg_quantity,
+  avg(f.base_price) AS avg_base_price,
+  avg(f.discount_percentage) AS avg_discount_percentage,
+  sum(f.order_item_count) AS order_item_count
 
-select 
-    f.return_status_code,
-    f.order_line_status_code,
-    sum(f.quantity) as quantity,
-    sum(f.gross_item_sales_amount) as gross_item_sales_amount,
-    sum(f.discounted_item_sales_amount) as discounted_item_sales_amount,
-    sum(f.net_item_sales_amount) as net_item_sales_amount,
+FROM fct_orders_items AS f
 
-    avg(f.quantity) as avg_quantity,
-    avg(f.base_price) as avg_base_price,
-    avg(f.discount_percentage) as avg_discount_percentage,
+WHERE f.ship_date <= {{ dbt_tpch.dateadd('day', -90,  var('max_ship_date')) }}
 
-    sum(f.order_item_count) as order_item_count
-    
-from
-    {{ ref('fct_orders_items') }} f
-where
-    f.ship_date <= {{ dbt_utils.dateadd('day', -90, var('max_ship_date')) }}
-group by
-    1,2    
+GROUP BY 
+  1, 2
