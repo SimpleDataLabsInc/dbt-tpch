6ab8dee1431512ba8c2922192507681e60356f8a
rajatgupta59@gmail.com <rajatgupta59@gmail.com>
2024-05-10 11:48:34 +0000

    🔁 Modified     - Source        - tpch     - Model        - fct_orders_items        - fct_orders        - orders

--- models/ods/orders.sql
+++ models/ods/orders.sql
@@ -1,23 +1,27 @@
 {{
-    config(
-        materialized = 'table'
-    )
+  config({    
+    "materialized": "table"
+  })
 }}
-with orders as (
 
-    select * from {{ ref('base_orders') }}
+WITH orders AS (
+
+  SELECT * 
+  
+  FROM {{ ref('base_orders')}}
 
 )
-select 
-    o.order_key, 
-    o.order_date,
-    o.customer_key,
-    o.order_status_code,
-    o.order_priority_code,
-    o.order_clerk_name,
-    o.shipping_priority,
-    o.order_amount
-from
-    orders o
-order by
-    o.order_date
+
+SELECT 
+  o.order_key,
+  o.order_date,
+  o.customer_key,
+  o.order_status_code,
+  o.order_priority_code,
+  o.order_clerk_name,
+  o.shipping_priority,
+  o.order_amount
+
+FROM orders AS o
+
+ORDER BY o.order_date
